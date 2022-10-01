947ba12426ff665f3922713085600484091e23c1
Jasurbek Nabijonov <97jasur@gmail.com>
2022-10-01 19:50:25 +0500

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timeline } from 'react-twitter-widgets';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Timeline
+                dataSource={{
+                    sourceType: 'list',
+                    ownerScreenName: 'twitter',
+                    id: '214727905'
+                }}
+                options={{
+                    height: '400',
+                    width: '400'
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
